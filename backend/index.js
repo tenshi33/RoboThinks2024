222bd838e297cb6243911efb66ce17985abbb465
Noirakumaaa <hernandezlnathaniel@gmail.com>
2024-08-12 00:59:31 +0800

    .env

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ import { fileURLToPath } from 'url';;
 
 const app = express();
 const PORT = 3000 ;
-const HOST = '127.0.0.1';
+const HOST = '0.0.0.0';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
