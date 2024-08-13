fe2b63b8030ee7c5dcd8d45aa7c75113f2bc2ed7
Noirakumaaa <hernandezlnathaniel@gmail.com>
2024-08-13 22:57:29 +0800

    edit vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // Port for the Vite development server
-    proxy: {
-      '/api': 'http://localhost:3000', // Proxy API requests to your Express server
-    },
-  },
   build: {
-    outDir: '../client/dist', // Directory to output build files
-    emptyOutDir: true, // Clear outDir before each build
+    outDir: 'public', // Make sure this matches your server's static file directory
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
   },
+  base: '/', // Adjust if needed
 });
